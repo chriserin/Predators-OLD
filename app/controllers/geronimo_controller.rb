624c3bdbfb0bb35026676c53253b240e5964a7ed
kilian <kilian_sweeney@yahoo.com>
2011-02-02 19:42:51 -0600

    Stripping content

--- app/controllers/geronimo_controller.rb
+++ app/controllers/geronimo_controller.rb
@@ -2,27 +2,27 @@ class GeronimoController < ApplicationController
 
   def everything
     @page_name = "Everything"
-    render :layout => 'g_application'
+    render 'example_site/everything', :layout => 'g_application'
   end
 
 
   def index
     blog = Blog.latest_for_frontpage
     @latest_front_page_blogpost = blog ? blog.blogtext : "Nothing to say"    
-    @next_show = Show.next_show  
-    render :layout => false
+    @next_show = Show.next_show
+    render 'example_site/index', :layout => false
   end
   
   def shows
     @page_name = "Shows"
     @upcoming_shows = Show.upcoming_shows
     @past_shows = Show.past_shows
-    render :layout => 'g_application'
+    render 'example_site/shows', :layout => 'g_application'
   end
   
   def blogs
     @page_name = "Says Things"
     @blogs = Blog.order("created_at DESC")
-    render :layout => 'g_application'    
+    render 'example_site/blogs', :layout => 'g_application'    
   end
 end
